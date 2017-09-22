59c10cd53ff1641cfa9d8a1c208e53ba6115860e
rafm0913 <rafm0913@gmail.com>
2017-09-22 16:06:55 +0800

    III_andriod_FinWK
    III_andriod_FinWK

---
+++ AndroidLittleHappy/LittleHappy/app/src/main/java/tw/org/iii/littlehappy/ActRegister.java
@@ -0,0 +1,13 @@
+package tw.org.iii.littlehappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actregister);
+    }
+}
