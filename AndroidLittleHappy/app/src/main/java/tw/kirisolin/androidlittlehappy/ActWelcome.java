a2a9c96b32ec8a9c0a0cea24538ff7161c1a4b77
rafm0913 <rafm0913@users.noreply.github.com>
2017-10-01 22:30:38 +0800

    add something about map
    try to add map ... but fail orz, wait Tai keep trying

--- AndroidLittleHappy/app/src/main/java/tw/kirisolin/androidlittlehappy/ActWelcome.java
+++ AndroidLittleHappy/app/src/main/java/tw/kirisolin/androidlittlehappy/ActWelcome.java
@@ -1,9 +1,13 @@
 package tw.kirisolin.androidlittlehappy;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 public class ActWelcome extends AppCompatActivity {
@@ -25,12 +29,31 @@ public class ActWelcome extends AppCompatActivity {
       }
     };
 
+    //check internet method
+    private boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actwelcome);
         Initialcomponent();
+
         mHandler.sendEmptyMessageDelayed(GoActMain, 2000);
+
     }
     private void Initialcomponent(){
         btnLogin=(Button)findViewById(R.id.btnLogin);
