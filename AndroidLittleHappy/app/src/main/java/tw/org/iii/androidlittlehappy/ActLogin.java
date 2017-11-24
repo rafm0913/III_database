c19ca240082da7ffbfe0a93256379f1998053652
rafm0913 <rafm0913@gmail.com>
2017-11-24 16:35:24 +0800

    log in 會比對SQL，只有留空會跳過SQL，11/28刪除留空跳過的功能

--- AndroidLittleHappy/app/src/main/java/tw/org/iii/androidlittlehappy/ActLogin.java
+++ AndroidLittleHappy/app/src/main/java/tw/org/iii/androidlittlehappy/ActLogin.java
@@ -25,8 +25,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ActLogin extends AppCompatActivity {
-    String URL = "http://52.198.163.90:8080/DemoServer/UrlController?action=" + "UserLogIn";
-
+    public static String URL = "http://52.198.163.90:8080/DemoServer/UrlController?action=" + "UserLogin";
+    public static String loginState = "";
     CCustomerFactory factory = new CCustomerFactory();
 
     private View.OnClickListener btnLogin_click= new View.OnClickListener() {
@@ -35,33 +35,52 @@ public class ActLogin extends AppCompatActivity {
 
 
             //按下登入
-            Job1 task = new Job1();
-            task.execute(new String[] { URL });
+            if (txtUserName.getText().toString().length()<1 || txtpassWD.getText().toString().length()<1)
+            {
+                Toast.makeText(ActLogin.this, "登入測試用空白帳號，11/28移除空白帳號，如未刪除call伯誠", Toast.LENGTH_SHORT).show();
+                //空白登入帳號，11/28移除 start
+                Intent intent=new Intent(ActLogin.this,ActMain.class);
+                String userID = txtUserName.getEditableText().toString();
+                SharedPreferences setting=getSharedPreferences("loginInfo",MODE_PRIVATE);
+                setting.edit()
+                        .putString(CDictionary.BK_LOGIN_INFOR_ID,userID)
+                        .commit();
+                CPublicParameters.user.setfUserName(userID);
+                startActivity(intent);
+                //空白登入帳號，11/28移除 end
+                //以下正式版本 11/28移除註解
+//                Toast.makeText(ActLogin.this, "帳號/密碼不得為空，請確認", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Job1 task = new Job1();
+                task.execute(new String[]{URL});
+            }
+
 
 
             //這段之後應該改為SQL語法
-            for (CCustomers C:factory.GetAll())
-            {
-                if (C.getfUserName().equals(txtUserName.getText().toString()))
-                {
-                    if (C.getfPassword().equals(txtpassWD.getText().toString()))
-                    {
-                        Intent intent=new Intent(ActLogin.this,ActMain.class);
-
-                        String userID = "";
-                        userID = txtUserName.getEditableText().toString();
-                        SharedPreferences setting=getSharedPreferences("loginInfo",MODE_PRIVATE);
-                        setting.edit()
-                                .putString(CDictionary.BK_LOGIN_INFOR_ID,userID)
-                                .commit();
-                        CPublicParameters.user.setfUserName(userID);
-                        startActivity(intent);
-                    }
-                    break;
-                }
-                txtpassWD.setText("");
-                Toast.makeText(ActLogin.this, "使用者帳號/密碼不符",Toast.LENGTH_SHORT).show();
-            }
+//            for (CCustomers C:factory.GetAll())
+//            {
+//                if (C.getfUserName().equals(txtUserName.getText().toString()))
+//                {
+//                    if (C.getfPassword().equals(txtpassWD.getText().toString()))
+//                    {
+//                        Intent intent=new Intent(ActLogin.this,ActMain.class);
+//
+//                        String userID = "";
+//                        userID = txtUserName.getEditableText().toString();
+//                        SharedPreferences setting=getSharedPreferences("loginInfo",MODE_PRIVATE);
+//                        setting.edit()
+//                                .putString(CDictionary.BK_LOGIN_INFOR_ID,userID)
+//                                .commit();
+//                        CPublicParameters.user.setfUserName(userID);
+//                        startActivity(intent);
+//                    }
+//                    break;
+//                }
+//                txtpassWD.setText("");
+//                Toast.makeText(ActLogin.this, "使用者帳號/密碼不符",Toast.LENGTH_SHORT).show();
+//            }
 
 
         }
@@ -188,8 +207,16 @@ public class ActLogin extends AppCompatActivity {
 
                 JSONArray ary = new JSONArray();
                 JSONObject obj = new JSONObject();
+                obj.put("fID", 99);
                 obj.put("fUserName", cust.getfUserName());
                 obj.put("fPassword", cust.getfPassword());
+                obj.put("fEmail", "");
+                obj.put("fNickName", "");
+                obj.put("fMascot", "");
+                obj.put("fDefaultStar", "");
+                obj.put("fDefaultTime", "");
+                obj.put("fStar", "");
+
                 ary.put(obj);
 
                 String params = String.format("json=%s", ary.toString());
@@ -215,14 +242,28 @@ public class ActLogin extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             //super.onPreExecute();
-            Toast.makeText(ActLogin.this, "背景工作開始執行", Toast.LENGTH_LONG).show();
+            //Toast.makeText(ActLogin.this, "背景工作開始執行", Toast.LENGTH_LONG).show();
         }
 
         //背景工作執行之後
         @Override
         protected void onPostExecute(String output) {
             //super.onPostExecute(output);
-            Toast.makeText(ActLogin.this, "背景工作執行完成\n" + output, Toast.LENGTH_SHORT).show();
+            loginState = output;
+            //Toast.makeText(ActLogin.this, "背景工作執行完成\n" + output, Toast.LENGTH_SHORT).show();
+            if ("1".equals(loginState)) {
+                Toast.makeText(ActLogin.this, "登入成功，轉接中，請稍後", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ActLogin.this, ActMain.class);
+                String userID = txtUserName.getEditableText().toString();
+                SharedPreferences setting = getSharedPreferences("loginInfo", MODE_PRIVATE);
+                setting.edit()
+                        .putString(CDictionary.BK_LOGIN_INFOR_ID, userID)
+                        .commit();
+                CPublicParameters.user.setfUserName(userID);
+                startActivity(intent);
+            } else {
+                Toast.makeText(ActLogin.this, "登入失敗，請確認帳號/密碼", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
